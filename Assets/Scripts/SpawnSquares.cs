a290087f3720b8e7fc87b4c3a2125cf0e411521d
Spencer337 <spencer.m.walter@gmail.com>
2025-09-07 19:02:43 -0400

    Created a new scene and worked on the Square Spawner task, all aspects are functional except the spawned squares do not stay on screen forever, only for about 100 seconds.

---
+++ Assets/Scripts/SpawnSquares.cs
@@ -0,0 +1,49 @@
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class SpawnSquares : MonoBehaviour
+{
+    public float radius;
+    public Vector2 start;
+    public Vector2 end;
+    public Vector2 mousePos;
+    public Color semiTransparent = new Color (1f, 1f, 1f, 0.5f);
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        radius = 0.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        radius += Input.mouseScrollDelta.y * 0.1f;
+        DrawSquare(semiTransparent, 0);
+
+        if (Input.GetMouseButton(0))
+        {
+            DrawSquare(Color.white, 100);
+        }
+    }
+
+    public void DrawSquare(Color c, int d)
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        start.x = mousePos.x - radius;
+        start.y = mousePos.y + radius;
+        end.x = mousePos.x + radius;
+        end.y = mousePos.y + radius;
+        Debug.DrawLine(start, end, c, d);
+        start = end;
+        end.y = end.y - radius - radius;
+        Debug.DrawLine(start, end, c, d);
+        start = end;
+        end.x = end.x - radius - radius;
+        Debug.DrawLine(start, end, c, d);
+        start = end;
+        end.y = end.y + radius + radius;
+        Debug.DrawLine(start, end, c, d);
+    }
+}
