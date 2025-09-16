36f0712ace0dd45f4d27be1e9c0b771eb5d11661
waltespe <waltespe@OA-S141-13>
2025-09-16 12:57:27 -0400

    Worked on journal 3  assignment in class, currently errors in stars script

--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using JetBrains.Annotations;
+using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -10,15 +11,24 @@ public class Player : MonoBehaviour
     public GameObject bombPrefab;
     public Transform bombsTransform;
     public Vector2 offset = new Vector2(0,1);
-    public float speed = 0.5f;
+    public float warpSpeed = 0.5f;
+    public float maxSpeed = 5;
+    public float acceleration;
+    public float accelerationTime = 3;
     public float bombSpacing = -0.2f;
     public int numberOfTrailBombs = 3;
+    public Vector3 velocity = new Vector3(0, 0, 0);
 
+    private void Start()
+    {
+        acceleration = maxSpeed / accelerationTime;
+    }
     void Update()
     {
+        PlayerMovement();
         if (Input.GetKeyDown(KeyCode.W))
         {
-            WarpPlayer(enemyTransform, speed);
+            WarpPlayer(enemyTransform, warpSpeed);
         }
         if (Input.GetKeyDown(KeyCode.B))
         {
@@ -36,6 +46,94 @@ public class Player : MonoBehaviour
         {
             DetectAsteroids(5f, asteroidTransforms);
         }
+    }
+
+    public void PlayerMovement()
+    {
+        //Way 1
+        //transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime, 0, 0);
+        //transform.Translate(0, Input.GetAxis("Vertical") * Time.deltaTime, 0);
+
+        // Way 2
+        //velocity.x = Input.GetAxisRaw("Horizontal");
+        //velocity.y = Input.GetAxisRaw("Vertical");
+        //transform.position += velocity * Time.deltaTime;
+
+        // Way 3
+        //if (Input.GetKey(KeyCode.RightArrow))
+        //{
+        //    velocity += Vector3.right * Time.deltaTime;
+        //    transform.position += velocity * Time.deltaTime;
+        //}
+        //if (Input.GetKey(KeyCode.LeftArrow))
+        //{
+        //    velocity += Vector3.left * Time.deltaTime;
+        //    transform.position += velocity * Time.deltaTime;
+        //}
+        //if (Input.GetKey(KeyCode.UpArrow))
+        //{
+        //    velocity += Vector3.up * Time.deltaTime;
+        //    transform.position += velocity * Time.deltaTime;
+        //}
+        //if (Input.GetKey(KeyCode.DownArrow))
+        //{
+        //    velocity += Vector3.down * Time.deltaTime;
+        //    transform.position += velocity * Time.deltaTime;
+        //}
+        //if (Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.LeftArrow))
+        //{
+        //    velocity.x = 0;
+        //}
+        //if (Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.DownArrow))
+        //{
+        //    velocity.y = 0;
+        //}
+        //if (velocity.x > maxSpeed)
+        //{
+        //    velocity.x = maxSpeed;
+        //}
+        //if (velocity.y > maxSpeed)
+        //{
+        //    velocity.y = maxSpeed;
+        //}
+        //if (velocity.x < -maxSpeed)
+        //{
+        //    velocity.x = -maxSpeed;
+        //}
+        //if (velocity.y < -maxSpeed)
+        //{
+        //    velocity.y = -maxSpeed;
+        //}
+
+        Vector3 direction = Vector3.zero;
+        // Move the object with acceleration logic
+        // When right key is pressed, increase the direction to the right
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            direction += Vector3.right;
+        }
+        // When left key is pressed, increase the direction to the left
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            direction += Vector3.left;
+        }
+        // When up key is pressed, increase the direction to the up
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            direction += Vector3.up;
+        }
+        // When down key is pressed, increase the direction to the down
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            direction += Vector3.right;
+        }
+        // Normalize direction vector
+        direction = Vector3.Normalize(direction);
+        velocity += (Vector3)direction * acceleration * Time.deltaTime;
+        transform.position += velocity * Time.deltaTime;
+
+        //Somewhere in your code, you will subtract the deceleration
+        //When you stop pressing input
     }
 
     public void SpawnBombAtOffset(Vector2 inOffset)
--- Assets/Scripts/Controllers/Stars.cs
+++ Assets/Scripts/Controllers/Stars.cs
@@ -6,9 +6,25 @@ public class Stars : MonoBehaviour
 {
     public List<Transform> starTransforms;
     public float drawingTime;
+    public float drawingDistance;
 
     // Update is called once per frame
     void Update()
     {
+        DrawConstellation();
+    }
+    public void DrawConstellation()
+    {
+        //for (int i = 0; i < starTransforms.Count-1; i++)
+        //{
+        //    Vector3 startPosition = starTransforms[i].position;
+        //    Vector3 endPosition = starTransforms[i + 1].position;
+        //    Debug.DrawLine(startPosition, endPosition);
+        //}
+        Vector3 startPosition = starTransforms[0].position;
+        Vector3 endPosition = starTransforms[1].position;
+        endPosition = (endPosition + startPosition).normalized * drawingDistance;
+        drawingDistance += drawingDistance;
+
     }
 }
