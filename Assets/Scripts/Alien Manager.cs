7025a0a18c4be3ab9a35ccc99cbf315b5b39062d
Spencer337 <spencer.m.walter@gmail.com>
2025-10-12 20:34:18 -0400

    Updated Alien Manager script to have the aliens properly rotate around the player at a consistent speed. The maintain their equal distance from one another while rotating. Additionally, the alien's not point towards the player.

--- Assets/Scripts/Alien Manager.cs
+++ Assets/Scripts/Alien Manager.cs
@@ -12,6 +12,8 @@ public class AlienManager : MonoBehaviour
     public List<Vector2> rotatingPoints;
 
     public float radius = 1.5f;
+    public float rotationSpeed = 2.0f;
+    public float rotationOffset = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -40,7 +42,7 @@ public class AlienManager : MonoBehaviour
 
     public void touchingPlayer ()
     {
-        for (int i = 0;i < aliens.Count; i++)
+        for (int i = 0; i < aliens.Count; i++)
         {
             float distance = Vector2.Distance(playerTransform.position, aliens[i].transform.position);
             if (distance <= 2)
@@ -56,12 +58,27 @@ public class AlienManager : MonoBehaviour
         // Gain all the points that the aliens will be at
         for (int i = 0; i < rotatingAliens.Count; i++)
         {
+            // Add new point to the list of rotationPoints
             float angle = (360 / rotatingAliens.Count) * i;
+            angle += rotationOffset;
             float angleInRadians = angle * Mathf.Deg2Rad;
             float x = Mathf.Cos(angleInRadians);
             float y = Mathf.Sin(angleInRadians);
             Vector2 newPoint = new Vector2(x, y) * radius;
             rotatingPoints.Add(newPoint);
+
+            // Have the rotating aliens to point towards the player
+            Vector3 directionToPlayer = playerTransform.position - rotatingAliens[i].transform.position;
+            float currentAngle = Mathf.Rad2Deg * Mathf.Atan2(rotatingAliens[i].transform.up.y, rotatingAliens[i].transform.up.x);
+            float targetAngle = Mathf.Rad2Deg * Mathf.Atan2(directionToPlayer.y, directionToPlayer.x);
+            float angleRemaining = Mathf.DeltaAngle(currentAngle, targetAngle);
+            rotatingAliens[i].transform.Rotate(0f, 0f, angleRemaining);
+        }
+
+        rotationOffset += rotationSpeed * Time.deltaTime;
+        if (rotationOffset >= 360)
+        {
+            rotationOffset = 0;
         }
 
         // Assign those points to the aliens currently rotating around the player
